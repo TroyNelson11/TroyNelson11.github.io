5d2475bfb4c890f5b1e6573853cd4fc11a4aebbc
TroyNelson11 <troyanelson11@gmail.com>
2025-01-26 14:17:44 -0600

    Auto-update player stats

--- scoring.js
+++ scoring.js
@@ -1796,8 +1796,8 @@ const player_stats = {
     "week 3": {
       "Rank": 0,
       "Player": "Zane Gonzalez",
-      "FPTS": 0,
-      "FPTS/G": 0,
+      "FPTS": 3,
+      "FPTS/G": 3,
       "ROST": "17.9%",
       "POS": "k"
     }
@@ -2596,14 +2596,14 @@ const player_stats = {
     "week 3": {
       "Rank": 0,
       "Player": "Jayden Daniels",
-      "CMP": 6,
-      "ATT": 9,
-      "PCT": 66.7,
-      "YDS": 33,
+      "CMP": 7,
+      "ATT": 12,
+      "PCT": 58.3,
+      "YDS": 39,
       "TD": 0,
       "INT": 0,
-      "ATT.1": 1,
-      "YDS.1": 1,
+      "ATT.1": 2,
+      "YDS.1": 6,
       "TD.1": 0,
       "FL": 0,
       "REC": 0,
@@ -2611,8 +2611,8 @@ const player_stats = {
       "YDS.2": 0,
       "TD.2": 0,
       "G": 1,
-      "FPTS": 1.4,
-      "FPTS/G": 1.4,
+      "FPTS": 2.2,
+      "FPTS/G": 2.2,
       "ROST": "7.7%",
       "POS": "rb"
     }
@@ -3110,7 +3110,7 @@ const player_stats = {
       "TD": 0,
       "INT": 0,
       "ATT.1": 2,
-      "YDS.1": 7,
+      "YDS.1": 8,
       "TD.1": 0,
       "FL": 0,
       "REC": 0,
@@ -3118,8 +3118,8 @@ const player_stats = {
       "YDS.2": 0,
       "TD.2": 0,
       "G": 1,
-      "FPTS": 0.7,
-      "FPTS/G": 0.7,
+      "FPTS": 0.8,
+      "FPTS/G": 0.8,
       "ROST": "5.1%",
       "POS": "rb"
     }
@@ -3180,19 +3180,19 @@ const player_stats = {
       "YDS": 0,
       "TD": 0,
       "INT": 0,
-      "ATT.1": 0,
-      "YDS.1": 0,
+      "ATT.1": 1,
+      "YDS.1": 1,
       "TD.1": 0,
       "FL": 0,
-      "REC": 0,
-      "TGT": 0,
-      "YDS.2": 0,
+      "REC": 1,
+      "TGT": 3,
+      "YDS.2": 6,
       "TD.2": 0,
       "G": 1,
-      "FPTS": 0.0,
-      "FPTS/G": 0.0,
+      "FPTS": 1.7,
+      "FPTS/G": 1.7,
       "ROST": "5.1%",
-      "POS": "qb"
+      "POS": "wr"
     }
   },
   "Jordan Addison": {
