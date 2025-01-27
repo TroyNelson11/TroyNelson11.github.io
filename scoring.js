25a0c8a000cfed4daf10dd8bec0034c75261485e
TroyNelson11 <troyanelson11@gmail.com>
2025-01-26 19:47:23 -0600

    Auto-update player stats

--- scoring.js
+++ scoring.js
@@ -1858,8 +1858,8 @@ const player_stats = {
       "YDS": 0,
       "TD": 0,
       "INT": 0,
-      "ATT.1": 4,
-      "YDS.1": 23,
+      "ATT.1": 6,
+      "YDS.1": 61,
       "TD.1": 1,
       "FL": 0,
       "REC": 1,
@@ -1867,8 +1867,8 @@ const player_stats = {
       "YDS.2": 17,
       "TD.2": 0,
       "G": 1,
-      "FPTS": 11.0,
-      "FPTS/G": 11.0,
+      "FPTS": 14.8,
+      "FPTS/G": 14.8,
       "ROST": "17.9%",
       "POS": "qb"
     }
