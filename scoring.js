6972e6b33db0fb5e0dea0068b2e5796253869839
TroyNelson11 <troyanelson11@gmail.com>
2026-01-18 14:40:32 -0600

    Auto-update player stats

--- scoring.js
+++ scoring.js
@@ -399,7 +399,7 @@ const player_stats = {
     "week 2": {
       "Rank": 0,
       "Player": "Houston Texans",
-      "SACKS": 0,
+      "SACKS": 1.0,
       "FR": 0,
       "INT.1": 0,
       "TD.3": 0,
@@ -407,8 +407,8 @@ const player_stats = {
       "SAFE": 0,
       "PA": 7,
       "G": 1,
-      "FPTS": 4,
-      "FPTS/G": 4,
+      "FPTS": 5.0,
+      "FPTS/G": 5.0,
       "ROST": 63.6,
       "POS": "def"
     }
