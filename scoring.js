65430da4cc88325258c974b14a1f3f5a3f6793b7
TroyNelson11 <troyanelson11@gmail.com>
2026-01-18 15:25:44 -0600

    Auto-update player stats

--- scoring.js
+++ scoring.js
@@ -734,10 +734,10 @@ const player_stats = {
     "week 2": {
       "Rank": 0,
       "Player": "Drake Maye",
-      "CMP": 6,
-      "ATT": 11,
-      "PCT": 54.5,
-      "YDS": 59,
+      "CMP": 8,
+      "ATT": 13,
+      "PCT": 61.5,
+      "YDS": 102,
       "TD": 1,
       "INT": 0,
       "ATT.1": 2,
@@ -749,8 +749,8 @@ const player_stats = {
       "YDS.2": 0,
       "TD.2": 0,
       "G": 1,
-      "FPTS": 5.6,
-      "FPTS/G": 5.6,
+      "FPTS": 7.3,
+      "FPTS/G": 7.3,
       "ROST": 40.9,
       "POS": "qb"
     }
@@ -2674,8 +2674,8 @@ const player_stats = {
       "YDS": 0,
       "TD": 0,
       "INT": 0,
-      "ATT.1": 5,
-      "YDS.1": 7,
+      "ATT.1": 7,
+      "YDS.1": 13,
       "TD.1": 0,
       "FL": 0,
       "REC": 1,
@@ -2683,8 +2683,8 @@ const player_stats = {
       "YDS.2": -2,
       "TD.2": 0,
       "G": 1,
-      "FPTS": 1.5,
-      "FPTS/G": 1.5,
+      "FPTS": 2.1,
+      "FPTS/G": 2.1,
       "ROST": 4.5,
       "POS": "qb"
     }
